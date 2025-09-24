6ca9e2aeb6a1fce5c52395c8b7480b2af9f953d6
Hugo-CASTELL <hugo.castell@outlook.fr>
2025-09-24 14:42:20 +0200

    Script v3 cleaning and running nohup everytime

--- nomorediscordupdatedownload.sh
+++ nomorediscordupdatedownload.sh
@@ -2,13 +2,15 @@
 
 url="https://discord.com/api/download/stable?platform=linux&format=deb" 
 
-localversion=$(cat /usr/share/discord/resources/build_info.json | jq .version | sed 's/"//g');
+localversion=$(cat /usr/share/discord/resources/build_info.json | jq .version | sed 's/"//g' | tr -d '\r\n');
 
-onlineversion=$(curl -sI "$url" | grep location | awk '{print $2}' | sed -E 's|.*discord-([0-9.]+)\.deb|\1|');
+onlineversion=$(curl -sI "$url" | grep location | awk '{print $2}' | sed -E 's|.*discord-([0-9.]+)\.deb|\1|' | tr -d '\r\n');
 
-if [ "$(echo "$localversion" | tr -d '\r\n')" = "$(echo "$onlineversion" | tr -d '\r\n')" ]; then
-  discord;
-else
+if [ "$localversion" != "$onlineversion" ]; then
   curl -L --output install.deb "$url";
-  sudo apt install ./install.deb -y && nohup discord >/dev/null 2>&1 &
+  sudo apt install ./install.deb -y;
+  rm -f ./install.deb;
 fi;
+
+nohup discord >/dev/null 2>&1 &
+
